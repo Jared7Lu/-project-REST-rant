663c92e7b4e7d7b67b141a63512a4bf9894b4165
jalp21@icloud.com <jalp21@icloud.com>
2022-12-03 13:12:49 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,19 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 //get places new
@@ -12,20 +23,7 @@ router.get('/new', (req, res) => {
 
 //get places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/rest1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/rest2.jpg'
-      }]
-      res.render('places/index', { places })
+      res.render('places/index', {places })
 })
 
 module.exports = router
