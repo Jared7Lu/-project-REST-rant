11b53f23a1ce4ea2afe3037bd02d24d11b31599c
jalp21@icloud.com <jalp21@icloud.com>
2022-12-02 16:15:31 -0800

    Getting index page working

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.render('Home')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.render('error404')
-  })
-  
+  res.render('error404')
+})
+
 app.listen(process.env.PORT)
+
---
+++ views/places/index.jsx
@@ -0,0 +1,12 @@
+// GET /places
+const React = require('react')
+
+function index () {
+  return (
+        <main>
+            <h1>Index page</h1>
+            <p>trial</p>
+        </main>
+  )
+}
+module.exports = index
